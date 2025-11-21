5f61f48b1247e2d6a2a7e73dde97f8b8cb28d0bd
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-11-21 05:27:51 +0100

    ✨ Cast Int

--- src/int.v
+++ src/int.v
@@ -1,13 +1,6 @@
 module val
 
-pub type Int = u8
-	| u16
-	| u32
-	| u64
-	| i8
-	| i16
-	| int // alias i32
-	| i64
+pub type Int = u8 | u16 | u32 | u64 | i8 | i16 | int | i64
 
 pub fn (i Int) uint() Uint {
 	return match i {
@@ -25,6 +18,72 @@ pub fn (i Int) uint() Uint {
 		}
 		i64 {
 			Uint(u64(i))
+		}
+	}
+}
+
+pub fn (i Int) u8() u8 {
+	return i.uint().u8()
+}
+
+pub fn (i Int) u16() u16 {
+	return i.uint().u16()
+}
+
+pub fn (i Int) u32() u32 {
+	return i.uint().u32()
+}
+
+pub fn (i Int) u64() u64 {
+	return i.uint().u64()
+}
+
+pub fn (i Int) i8() i8 {
+	return if i is i8 {
+		i
+	} else {
+		i as i8
+	}
+}
+
+pub fn (i Int) i16() i16 {
+	return match i {
+		i8 {
+			i16(i)
+		}
+		i16 {
+			i
+		}
+		else {
+			i as i16
+		}
+	}
+}
+
+pub fn (i Int) int() int {
+	return match i {
+		i8, i16 {
+			int(i)
+		}
+		int {
+			i
+		}
+		else {
+			i as int
+		}
+	}
+}
+
+pub fn (i Int) i64() i64 {
+	return match i {
+		i8, i16, int {
+			i64(i)
+		}
+		i64 {
+			i
+		}
+		else {
+			i as i64
 		}
 	}
 }
