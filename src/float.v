d44641f6727ae95d083cc1da5653d50b77199633
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-11-21 05:49:14 +0100

    ✨ Cast Float

--- src/float.v
+++ src/float.v
@@ -1,3 +1,22 @@
 module val
 
 pub type Float = f32 | f64
+
+pub fn (f Float) f32() f32 {
+	return if f is f32 {
+		f
+	} else {
+		f as f32
+	}
+}
+
+pub fn (f Float) f64() f64 {
+	return match f {
+		f32 {
+			f64(f)
+		}
+		f64 {
+			f
+		}
+	}
+}
