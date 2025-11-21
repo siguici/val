5954ca7ac45d89bd4bcef34ba24105d284c62ef3
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-11-21 05:16:12 +0100

    ✨ Cast Int to Uint

--- src/int.v
+++ src/int.v
@@ -8,3 +8,23 @@ pub type Int = u8
 	| i16
 	| int // alias i32
 	| i64
+
+pub fn (i Int) uint() Uint {
+	return match i {
+		u8, u16, u32, u64 {
+			Uint(i)
+		}
+		i8 {
+			Uint(u8(i))
+		}
+		i16 {
+			Uint(u16(i))
+		}
+		int {
+			Uint(u32(i))
+		}
+		i64 {
+			Uint(u64(i))
+		}
+	}
+}
