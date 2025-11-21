71c19c225e50ed1271aa686ac308dd881c5fc5f3
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-11-21 06:05:01 +0100

    ✨ Cast Number to Uint

--- src/number.v
+++ src/number.v
@@ -1,3 +1,17 @@
 module val
 
 pub type Number = u8 | u16 | u32 | u64 | i8 | i16 | int | i64 | f32 | f64
+
+pub fn (n Number) uint() Uint {
+	return match n {
+		u8, u16, u32, u64 {
+			Uint(n)
+		}
+		i8, i16, int, i64 {
+			Int(n).uint()
+		}
+		f32, f64 {
+			Float(n).uint()
+		}
+	}
+}
